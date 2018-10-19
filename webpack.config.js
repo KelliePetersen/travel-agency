40d24f70b80017cae50516fe05cc2e188028170b
KelliePetersen <KellieAnnPetersen@gmail.com>
2018-10-19 01:34:11 -0400

    add lazyloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
   output: {
     path: path.resolve(__dirname, "./app/temp/scripts"),
-    filename: "App.js"
+    filename: "[name].js"
   },
   module: {
     rules: [
@@ -12,9 +15,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"]
           }
         }
       }
