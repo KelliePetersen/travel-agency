fc685eec22630de7b361140db29f182198515985
KelliePetersen <KellieAnnPetersen@gmail.com>
2018-10-18 21:45:27 -0400

    add webpack, integrate webpack into gulp, add babel

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,15 @@
+const $ = require('jquery');
+import Person from './modules/Person';
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in taxes.");
+  }
+}
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Adult("Jane Smith", "green");
+jane.greet();
+jane.payTaxes();
